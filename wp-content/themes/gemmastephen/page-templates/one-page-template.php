7a680c250d273767fc4c313eaa4327494857e131
Gemma Stephen <gemma.stephen@planysdigital.com>
2016-09-05 17:12:10 +0100

    Updated portfolio modals

--- wp-content/themes/gemmastephen/page-templates/one-page-template.php
+++ wp-content/themes/gemmastephen/page-templates/one-page-template.php
@@ -129,9 +129,23 @@
 					<div class="col-sm-10 col-sm-offset-1">
 	                    <div class="row">
 
+	                    	<div class="job-details clearfix">
+				                <div class="col-sm-4">
+				                    <h2>Sept 16 – Present</h2> 
+				                    <h3>E-Com Scotland, Dunfermline</h3>
+				                    <p>Front-end Designer</p>
+				                </div>
+
+				                <div class="col-sm-8">
+				                    <p class="description">
+				                    	<strong>My role:</strong> Info coming soon.
+				                    </p>
+				                </div>
+				            </div>
+
 							<div class="job-details clearfix">
 				                <div class="col-sm-4">
-				                    <h2>July 12 – Present</h2> 
+				                    <h2>July 12 – September 16</h2> 
 				                    <h3>Planys Mobile, Dunfermline</h3>
 				                    <p>Front-end Developer</p>
 				                </div>
@@ -143,6 +157,7 @@
 				                    </p>
 				                </div>
 				            </div>
+
 							<div class="job-details clearfix">
 								<div class="col-sm-4">
 				                    <h2>May 11 – July 12</h2> 
@@ -153,7 +168,7 @@
 									<p class="description">
 										<strong>My role:</strong> Initially employed as an intern for 1 month testing and creating websites and then progressed into full-time gaining more knowledge and skills in the field of web development. 
 										The role included UI design and CMS integration, managing and updating current client sites using technologies such as HTML, CSS, Javascript, PHP.
-										As part of this role, I also built relationships with clients and assisted operations between clients and developers. QA tested on web and mobile based applications, creating test documents and reporting and managing bugs
+										As part of this role, I also built relationships with clients and assisted operations between clients and developers. QA tested on web and mobile based applications, creating test documents and reporting and managing bugs.
 									</p>
 				                </div>
 							</div>
@@ -179,7 +194,7 @@
 	                <div class="col-sm-10 col-sm-offset-1 text-center">
 			            <div class="row">
 			                <div class="col-md-4 col-sm-6 portfolio-item">
-			                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+			                    <a href="#bh-modal" class="portfolio-link" data-toggle="modal">
 			                        <div class="portfolio-hover">
 			                            <div class="portfolio-hover-content">
 			                                <i class="fa fa-angle-double-right fa-3x"></i>
@@ -395,8 +410,8 @@
 
 	<!-- Portfolio Modals -->
 
-    <!-- Portfolio Modal 1 -->
-    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+    <!-- Business Hound -->
+    <div class="portfolio-modal modal fade" id="bh-modal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="close-modal" data-dismiss="modal">
@@ -409,17 +424,20 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                <h2>Business Hound</h2>
+                                <p class="item-intro text-muted">Brochure site &mp; web based app</p>
                                 <img class="img-responsive img-centered" src="img/portfolio/roundicons-free.png" alt="">
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
                                 <p>
-                                    <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p>
+	                                <strong>What I did:</strong> HTML, CSS, Javascript, SEO, Responsive Design
+	                            </p>
+                                <p>
+                                    Business Hound is a financial admin site for small businesses to manage invoicing, record mileage via an app and track expenses. 
+                                    This included development of a responsive brochure site and the main web app. 
+                                    I worked on the front-end of this Ruby on Rails project collaboratively with the designer and back-end developers.
+                                </p>
                                 <ul class="list-inline">
-                                    <li>Date: July 2014</li>
-                                    <li>Client: Round Icons</li>
-                                    <li>Category: Graphic Design</li>
+                                    <li>Date: April 2016</li>
+                                    <li>Client: Business Hound</li>
                                 </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
@@ -472,7 +490,6 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <!-- Project Details Go Here -->
                                 <h2>Project Name</h2>
                                 <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
                                 <img class="img-responsive img-centered" src="img/portfolio/treehouse-preview.png" alt="">
@@ -501,7 +518,6 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <!-- Project Details Go Here -->
                                 <h2>Project Name</h2>
                                 <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
                                 <img class="img-responsive img-centered" src="img/portfolio/golden-preview.png" alt="">
@@ -530,7 +546,6 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <!-- Project Details Go Here -->
                                 <h2>Project Name</h2>
                                 <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
                                 <img class="img-responsive img-centered" src="img/portfolio/escape-preview.png" alt="">
@@ -559,7 +574,6 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <!-- Project Details Go Here -->
                                 <h2>Project Name</h2>
                                 <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
                                 <img class="img-responsive img-centered" src="img/portfolio/dreams-preview.png" alt="">
